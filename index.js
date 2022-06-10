c96c5bbf29fce16c1f27ccef4b463b617e905132
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-06-10 13:04:40 +0300

    commit tree 73201ae2f754a7e37532c43c7ed23a031e0ed309

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if(feet <= 400){
+    return `This one is on me!`;
+  }
+  else if(feet > 2000 && feet < 2500){
+    return `I will gladly take your thirty bucks.`;
+  }
+  else if(feet > 2500){
+    return `No can do.`;
+  }
+
+}
+console.log(scuberGreetingForFeet(199))
+
+
+
+
+function ternaryCheckCity(city){
+  // Write your code here!
+  if (city === 'NYC'){
+    return `Ok, sounds good.`
+  }
+  else(city != 'NYC');{
+    return `No go`
+  }
+console.log(ternaryCheckCity('NYC'))
+
+
+function switchOnCharmFromTip(tip){
+  // Write your code here!
+  switch(tip){
+    case 'generous':
+    return `Thank you so much.`
+    case 'not as generous':
+    return `Thank you.`
+    default:
+      return `Bye.`
+  }
+
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
 }
